eec1995b5c06fcbd342bdc36e23968b25ee744a4
ancazugo <33363093+ancazugo@users.noreply.github.com>
2020-11-16 14:03:09 -0500

    Changed tile to watercolor

--- paleo_maps_script.py
+++ paleo_maps_script.py
@@ -5,12 +5,11 @@ from folium import Map, Marker, Tooltip
 df = pd.read_csv('study_sites.csv', sep=';')
 
 # token = open("mapbox_token.txt").read()
-paleo_map = Map(location=[4.740165, -73.980105], tiles='CartoDB positron', zoom_start=6)
+paleo_map = Map(location=[4.740165, -73.980105], tiles='Stamen Watercolor', zoom_start=6)
 
 for index, row in df.iterrows():
     researcher = row['Researcher']
     site = row['Site']
-    print(researcher, site)
-    Marker([row['Latitude'], row['Longitude']], tooltip=Tooltip(f'<p><b>{researcher}</b></p> <br> <p><b>{site}</b></p>',)).add_to(paleo_map)
+    Marker([row['Latitude'], row['Longitude']], tooltip=Tooltip(f'<p><b>{site}</b></p> <br> <p>{researcher}</p>',)).add_to(paleo_map)
 
 paleo_map.save('index.html')
