2b4d8af612ba995da3c8d419df2dfa4ef9604280
ancazugo <33363093+ancazugo@users.noreply.github.com>
2020-11-25 20:00:07 -0500

    added new images

---
+++ .ipynb_checkpoints/paleo_maps_script-checkpoint.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import folium
+from folium import Map, Marker, Tooltip
+
+df = pd.read_csv('study_sites.csv', sep=';')
+
+# token = open("mapbox_token.txt").read()
+paleo_map = Map(location=[4.740165, -73.980105], tiles='Stamen Terrain', zoom_start=5)
+
+for index, row in df.iterrows():
+    researcher = row['Researcher']
+    site = row['Site']
+    Marker([row['Latitude'], row['Longitude']], tooltip=Tooltip(f'<p><b>{site}</b></p> <br> <p>{researcher}</p>',)).add_to(paleo_map)
+
+paleo_map.save('index.html')
